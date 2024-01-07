12bae50c1cd9c72e86ab6bc18d5951109ac6cc1f
Manesh Karun R <68321124+maneshkarun@users.noreply.github.com>
2024-01-07 18:19:49 +0530

    Added ingest_data.py

---
+++ ingest_data.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db_name = params.db_name
+    table_name = params.table_name 
+    url = params.url
+
+    csv_name = 'output.csv.tz'
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db_name}")
+
+    df_iter = pd.read_csv(csv_name, compression='gzip', iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+
+    df.to_sql(name=table_name, con=engine, if_exists="append")
+
+
+    while True:
+        start_time = time()
+        df = next(df_iter)
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.to_sql(name=table_name, con=engine, if_exists="append")
+        end_time = time()
+        print(f"Chunk appended successfully in {end_time - start_time}")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('user', help='username for postgres')
+    parser.add_argument('password', help='password for postgres')
+    parser.add_argument('host', help='host for postgres')
+    parser.add_argument('port', help='port for postgres')
+    parser.add_argument('db_name', help='database name for postgres')
+    parser.add_argument('table_name', help='tablename for postgres')
+    parser.add_argument('url', help='url for the csv file')
+    args = parser.parse_args()
+    main(args)
